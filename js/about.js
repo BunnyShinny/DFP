a6d687269f7b72c7fcc2ce6213f767518cda87ad
Shine Wanna  Aung <shinewanna27199@gmail.com>
2022-06-01 13:58:59 +0630

    updating about us

---
+++ js/about.js
@@ -0,0 +1,26 @@
+$(function () {
+  // $('.owl-1').owlCarousel({
+
+  //    loop:true,
+  //    margin:0,
+  //    nav:true,
+  //    items: 1,
+  //    smartSpeed: 1000,
+  //    autoplay: true,
+  //    autoplayHoverPause: true,
+  //    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+  // })
+
+  if ($(".slide-one-item").length > 0) {
+    $(".slide-one-item").owlCarousel({
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 0,
+      autoplay: true,
+      animateIn: "animate__fadeIn",
+      animateOut: "animate__fadeOut",
+      pauseOnHover: false,
+    });
+  }
+});
